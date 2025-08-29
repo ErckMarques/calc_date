1215f290c2038252805c9aff32374fa289b46977
Erik Marques <lucroalternativo@outlook.com>
2025-08-29 10:38:48 -0300

    add constant ICON_PATH

--- src/date_calc/__init__.py
+++ src/date_calc/__init__.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import ttkbootstrap as ttk
+
+ICON_PATH: Path = Path(__file__).parents[1].joinpath("assets")
+
+type TkContainer = ttk.Window | ttk.Frame | ttk.Labelframe
+
 from date_calc.config import config_locale_app
 
 config_locale_app()
