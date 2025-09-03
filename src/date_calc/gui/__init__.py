e7c8c1b226f8d2d868ff64fa8f8ddc7badc37520
Erik Marques <lucroalternativo@outlook.com>
2025-09-03 12:26:09 -0300

    Removed arguments from __init__(), as definitions are made within the class itself. Removed unused imports.

--- src/date_calc/gui/__init__.py
+++ src/date_calc/gui/__init__.py
@@ -2,11 +2,8 @@ def main():
     from date_calc.gui.twindow import TWindow
     from date_calc.gui.frame_date_difference import FrameDateDifference
     from date_calc.gui.frame_data_interval import FrameDateWithInterval
-    from date_calc import config_locale_app
-
-    config_locale_app()
     
-    window = TWindow(title="Date Calculator", themename="darkly")
+    window = TWindow()
     window.mainloop()
 
 if __name__ == "__main__":
