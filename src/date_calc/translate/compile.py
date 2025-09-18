1930d6341d56b7195bf92e4314f025851d014a48
Erik Marques <lucroalternativo@outlook.com>
2025-09-18 15:30:54 -0300

    additions and corrections locals

--- src/date_calc/translate/compile.py
+++ src/date_calc/translate/compile.py
@@ -3,7 +3,7 @@ This module provides a function for compiling the '.mo' binary files, used by ge
 """
 import polib
 from pathlib import Path
-from typing import cast, TypeAlias
+from typing import cast, TypeAlias, Optional
 
 from date_calc.translate.raises import IsNotPOFileError
 
@@ -26,21 +26,39 @@ def _compile_po_2_mo(po_file: StrOrPathPOFile, domain: str = 'app'):
 
     if isinstance(file, Path):
         if not file.is_file() or not file.exists():
-            raise FileNotFoundError(f"Passed argument is either not a file or does not exist. \nPath: {file.as_posix()}")
+            msg = f"Passed argument is either not a file or does not exist. \nPath: {file.as_posix()}"
+            raise FileNotFoundError(msg)
         
-    if file.suffix == '.po':
-        raise IsNotPOFileError(f"The passed file is not of type '.po', \nPath: {file.as_posix()}")
+    if file.suffix != '.po':
+        msg = f"The passed file is not of type '.po', \nPath: {file.as_posix()}"
+        raise IsNotPOFileError(msg)
     
-    mo_file = file.as_posix().replace(".po", ".mo")
+    mo_file = file.with_suffix(".mo").as_posix()
     nfile = polib.pofile(file)
     nfile.save_as_mofile(mo_file)
 
+def compile_po_2_mo(po_file: Optional[StrOrPathPOFile] = None, domain: str = 'app'):
+    """
+    This function takes a string or a pathlib.Path object,
+    representing the path of the '.po' file and saves it in the same folder or path.
+
+    Args:
+        po_file (StrOrPathPOFile: TypeAlias = Path | str): file path
+        domain(str): file name
+    """
+    try:
+        if po_file is None:
+            # calc_dates\src\date_calc\locale
+            LOCALE_DIR = Path(__file__).parents[2].joinpath('locale')
+
+            # calc_dates\src\date_calc\locale\pt
+            for dir in LOCALE_DIR.iterdir():
+                # calc_dates\src\date_calc\locale\pt\LC_MESSAGES\app.po
+                _compile_po_2_mo(dir.joinpath('LC_MESSAGES', 'app.po'))
+        else:
+            _compile_po_2_mo(po_file, domain)
+    except Exception as e:
+        raise e
 
 if __name__ == "__main__":
-    # C:\Users\PC\python_projects\calc_dates\src\date_calc\locale
-    LOCALE_DIR = Path(__file__).parents[1].joinpath('locale')
-
-    # C:\Users\PC\python_projects\calc_dates\src\date_calc\locale\pt
-    for dir in LOCALE_DIR.iterdir():
-        # C:\Users\PC\python_projects\calc_dates\src\date_calc\locale\pt\LC_MESSAGENS\app.po
-        _compile_po_2_mo(dir.joinpath('LC_MESSAGENS', 'app.po'))
+    compile_po_2_mo()
--- src/date_calc/translate/translate.py
+++ src/date_calc/translate/translate.py
@@ -2,13 +2,14 @@ from gettext import GNUTranslations
 from pathlib import Path
 from typing import Literal
 
-LOCALE_PATH_DIR: str = Path(__file__).parents[2].joinpath('locale').as_posix()
+_DEFAULT_LOCALES_PATH: Path = Path(__file__).parents[1].joinpath('locale')
+DEFAULT_LOCALES_PATH: str = _DEFAULT_LOCALES_PATH.as_posix()
 
 def translate_with_gettext(lang: Literal['pt_BR', 'en_US']) -> GNUTranslations:
     import gettext
-    gettext.bindtextdomain('app', localedir=LOCALE_PATH_DIR)
+    gettext.bindtextdomain('app', localedir=DEFAULT_LOCALES_PATH)
     gettext.textdomain('app')
 
-    translate = gettext.translation('app', localedir=LOCALE_PATH_DIR, languages=lang)
+    translate = gettext.translation('app', localedir=DEFAULT_LOCALES_PATH, languages=[lang])
     
     return translate
