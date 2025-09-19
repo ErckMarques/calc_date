01387c01ce02610444e22cc464c3faf5b30ffb01
Erik Marques <lucroalternativo@outlook.com>
2025-09-19 13:36:33 -0300

    Added and fixed file compilation features and the translation system. Needs correction.

--- src/date_calc/translate/compile.py
+++ src/date_calc/translate/compile.py
@@ -7,9 +7,46 @@ from typing import cast, TypeAlias, Optional
 
 from date_calc.translate.raises import IsNotPOFileError
 
-StrOrPathPOFile: TypeAlias = Path | str
+StrOrPath: TypeAlias = Path | str
 
-def _compile_po_2_mo(po_file: StrOrPathPOFile, domain: str = 'app'):
+def _validate_locale_path(p: StrOrPath) -> Path:
+    """Validates that the path to the 'locale' folder is valid."""
+
+    if not isinstance(p, (str, Path)):
+        msg = "The 'path' argument must be of type 'str' or 'Path'."
+        raise TypeError(msg)
+    
+    if isinstance(p, str):
+        try:
+            p = Path(p).resolve()
+        except Exception as e:
+            msg = f"Error resolving path: {e}"
+            raise ValueError(msg)
+    
+    if not p.exists() or not p.is_dir():
+        msg = f"Invalid path: '{p}'. Directory does not exist or not an accessible directory."
+        raise FileNotFoundError(msg)
+
+    return p
+
+def _validate_pofile_path(p: StrOrPath) -> Path:
+    """Validates that the 'locale' directory structure complies with the 'gettext' specification."""
+
+    locale = _validate_locale_path(p)
+
+    pofile = locale.joinpath('LC_MESSAGES', 'app.po')
+
+    if not pofile.exists() or not pofile.is_file():
+        msg = f"Invalid path: '{p}'. File does not exist or is not an accessible file."
+        raise FileNotFoundError(msg)
+
+    if pofile.suffix != '.po':
+        msg = f"The passed file is not of type '.po', \nPath: {pofile.as_posix()}"
+        raise IsNotPOFileError(msg)
+
+    return pofile
+
+def _compile_po_2_mo(po_file: StrOrPath):
     """
     This function takes a string or a pathlib.Path object,
     representing the path of the '.po' file and saves it in the same folder or path.
@@ -19,35 +56,24 @@ def _compile_po_2_mo(po_file: StrOrPathPOFile, domain: str = 'app'):
         domain(str): file name
 
     """
-    if isinstance(po_file, str):
-        file = Path(po_file).resolve()
-
-    file = cast(Path, po_file)
-
-    if isinstance(file, Path):
-        if not file.is_file() or not file.exists():
-            msg = f"Passed argument is either not a file or does not exist. \nPath: {file.as_posix()}"
-            raise FileNotFoundError(msg)
+    file = _validate_locale_path(po_file)
         
-    if file.suffix != '.po':
-        msg = f"The passed file is not of type '.po', \nPath: {file.as_posix()}"
-        raise IsNotPOFileError(msg)
     
     mo_file: str = file.with_suffix(".mo").as_posix()
     nfile: polib.POFile = polib.pofile(file)
     nfile.save_as_mofile(mo_file)
 
-def compile_po_2_mo(po_file: Optional[StrOrPathPOFile] = None, domain: str = 'app'):
+def compile_po_2_mo(locale_path: StrOrPath, domain: str = 'app'):
     """
     This function takes a string or a pathlib.Path object,
     representing the path of the '.po' file and saves it in the same folder or path.
 
     Args:
-        po_file (StrOrPathPOFile: TypeAlias = Path | str): file path
+        locale_path (StrOrPath): Path to the 'locale' folder containing the .po files
         domain(str): file name
     """
     try:
-        if po_file is None:
+        if locale_path is None:
             # calc_dates\src\date_calc\locale
             LOCALE_DIR = Path(__file__).parents[2].joinpath('locale')
 
@@ -56,6 +82,11 @@ def compile_po_2_mo(po_file: Optional[StrOrPathPOFile] = None, domain: str = 'ap
                 # calc_dates\src\date_calc\locale\pt\LC_MESSAGES\app.po
                 _compile_po_2_mo(dir.joinpath('LC_MESSAGES', 'app.po'))
         else:
+            for dir in locale_path.iterdir():
+                po_file = dir.joinpath('LC_MESSAGES', f'{domain}.po')
+                if not po_file.exists():
+                    msg = f"No '.po' file found in the path: {po_file.as_posix()}"
+                    raise FileNotFoundError(msg)
             _compile_po_2_mo(po_file, domain)
     except Exception as e:
         raise e
