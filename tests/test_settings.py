6ce80032ef26d387e8462523eb74a36a57873a1c
Erik Marques <lucro.alternativo@outlook.com>
2025-10-06 23:15:11 -0300

    Added new tests for the dynaconf instance

---
+++ tests/test_settings.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import pytest
+from dynaconf import Dynaconf, ValidationError, LazySettings
+
+from date_calc import load_config
+
+@pytest.fixture(scope="module", autouse=True)
+def settings() -> LazySettings:
+    return load_config()
+
+def test_load_config(settings: LazySettings):
+    
+    assert settings is not None, "As configurações não foram devidamente carregadas."
+    # assert settings.current_env == "default", "O ambiente atual não é 'default'."
+    assert isinstance(settings, LazySettings), "O objeto de configurações não é uma instância de LazySettings."
+    assert settings.get("TIMEZONE", default=None) == "America/Recife", "A configuração TIMEZONE não está definida corretamente."
+    assert isinstance(settings.get("DEFAULT_LOCALES_PATH"), Path), "A configuração DEFAULT_LOCALES_PATH não está definida corretamente."
+    assert isinstance(settings.get("ASSETS_PATH"), Path), "A configuração ASSETS_PATH não está definida corretamente."
+
+def test_development_env_vars(settings: LazySettings):
+    # Testa se as variáveis de ambiente de desenvolvimento estão definidas corretamente
+    with settings.using_env("development"):
+        assert settings.get("DEBUG", default=False) is True, "A variável de ambiente DEBUG não está definida como True."
+        assert settings.get("LOG_LEVEL", default=False) is True, "A variável de ambiente LOG_LEVEL não está definida como True."
