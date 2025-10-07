8f1e2122fd50ffa5e11f3997a00bbf936a380e3b
Erik Marques <lucro.alternativo@outlook.com>
2025-10-06 23:13:11 -0300

    Additions and fixes for the dynaconf instance

--- src/date_calc/config.py
+++ src/date_calc/config.py
@@ -1,8 +1,11 @@
 import locale
 import sys
 from pathlib import Path
+import logging
 
-from dynaconf import Dynaconf, Validator
+from dynaconf import Dynaconf, Validator, ValidationError
+
+logger = logging.getLogger(__name__)
 
 def config_locale_app():
     """
@@ -22,25 +25,34 @@ def _validate_path(path: Path) -> Path:
     return path
 
 def load_config() -> Dynaconf:
-    ROOT_PATH = Path(__file__).parents[2].resolve()
+    ROOT_PATH = Path(__file__).parents[1].resolve()
+    SETTINGS_PATH = ROOT_PATH.joinpath("settings.toml")
+    
+    logger.debug(f"Root path for configuration: {ROOT_PATH}")
 
-    settings = Dynaconf(
-        settings_files= ROOT_PATH.joinpath("settings.toml"),
-        
-        # Prefixo para variáveis de ambiente
-        envvar_prefix="DATE_CALC",
-        
-        # Suporte a múltiplos ambientes
-        environments=True,
-        default_env='development',
-        
-        # Mescla variáveis de ambiente com arquivos
-        merge_enabled=True,
+    try:
+        logger.debug(f"SETTINGS path for configuration: {SETTINGS_PATH}, exists: {SETTINGS_PATH.exists()}")
+        settings = Dynaconf(
+            settings_files=[SETTINGS_PATH],
+            
+            # Prefixo para variáveis de ambiente
+            envvar_prefix="DTC",
+            
+            # Suporte a múltiplos ambientes
+            environments=True,
+            default_env='default',
+            
+            # Mescla variáveis de ambiente com arquivos
+            merge_enabled=True,
 
-        # Validações para garantir que certas configurações estejam presentes
-        validators=[
-            Validator("DEFAULT_LOCALES_PATH", must_exist=True, required=True, cast=Path),
-        ],
-    )
+            # Validações para garantir que certas configurações estejam presentes
+            validators=[
+                Validator("DEFAULT_LOCALES_PATH", must_exist=True, cast=Path),
+                Validator("ICON_PATH", must_exist=True, cast=Path),
+                Validator("TIMEZONE", must_exist=True, is_type_of=str),
+            ],
+        )
+    except ValidationError as e:
+        logger.exception("Error loading configuration: %s", e)
 
     return settings
